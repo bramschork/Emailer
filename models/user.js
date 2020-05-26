ab4347732674b96a9c3472416d2ab601224aa048
Bram Schork <bramscho@haverford.org>
2020-05-25 23:53:01 -0400

    moving stuff around

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose; //this line is equivalent: const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-  googleID: String
-});
-
-mongoose.model('users', userSchema);
